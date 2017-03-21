5d14c8b3e5ce02a231085165b1b7cd889d4e6714
riasc <richard.schaefer@ibvt.uni-stuttgart.de>
2017-03-21 18:35:10 +0100

    play

---
+++ play/play.py
@@ -0,0 +1,23 @@
+import webapp2
+
+form = """
+<form action="/testform">
+    <input name="q">
+    <input type="submit">
+</form>
+"""
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.out.write(form)
+
+class TestHandler(webapp2.RequestHandler):
+    def get(self):
+        q = self.request.get("q")
+        self.response.out.write(q) 
+        #self.response.headers['Content-Type'] = 'text/plain' 
+        #self.response.out.write(self.request)
+
+app = webapp2.WSGIApplication([('/', MainPage),
+                                ('/testform', TestHandler)], 
+                                debug=True)
