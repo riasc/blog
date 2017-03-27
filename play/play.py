5f63b677d0f08d735310200a8e33ababea464861
riasc <richard.schaefer@ibvt.uni-stuttgart.de>
2017-03-27 18:40:21 +0200

    POST method

--- play/play.py
+++ play/play.py
@@ -1,7 +1,7 @@
 import webapp2
 
 form = """
-<form action="/testform">
+<form method="post" action="/testform">
     <input name="q">
     <input type="submit">
 </form>
@@ -12,11 +12,9 @@ class MainPage(webapp2.RequestHandler):
         self.response.out.write(form)
 
 class TestHandler(webapp2.RequestHandler):
-    def get(self):
+    def post(self):
         q = self.request.get("q")
-        self.response.out.write(q) 
-        #self.response.headers['Content-Type'] = 'text/plain' 
-        #self.response.out.write(self.request)
+        self.response.out.write(q)
 
 app = webapp2.WSGIApplication([('/', MainPage),
                                 ('/testform', TestHandler)], 
